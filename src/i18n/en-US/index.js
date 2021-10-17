23cf2c50d7dca25b2ec29bd11295340456734fa6
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2021-10-17 14:25:23 -0400

    init

---
+++ src/i18n/en-US/index.js
@@ -0,0 +1,13 @@
+/**
+ * RULES:
+ *
+ * 1. Max depth: 3 Ex: messages: {errors: {some: 'Error'}}
+ * 2. No translation in the root level Ex: messages: {some: 'Error'}
+ * 3. use the main root keys common, messages, and actions.
+ */
+
+export default {
+  common: {},
+  messages: {},
+  actions: {},
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Cooking n Share logo"
+      src="~assets/images/logo-with-text.png"
+      style="width: 80%; height: 30%; max-width: 552px; max-height: 283px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
