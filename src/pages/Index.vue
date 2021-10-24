4820f64d5720b934cbac96ff01ccb48f81399e7d
rafieltq <taverasrafiel@gmail.com>
2021-10-24 13:11:58 -0400

    A title for the page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Cooking n Share logo"
-      src="~assets/images/logo-with-text.png"
-      style="width: 80%; height: 30%; max-width: 552px; max-height: 283px"
-    >
+    <h5>INDEX PAGE</h5>
   </q-page>
 </template>
 
