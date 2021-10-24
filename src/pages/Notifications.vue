2b4f9b7579da8e85fba1ed650b077015874372da
rafieltq <taverasrafiel@gmail.com>
2021-10-24 13:11:44 -0400

    pages for routes

---
+++ src/pages/Notifications.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h5>NOTHING YET</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Notifications'
+})
+</script>
---
+++ src/pages/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h5>SEARCH PAGE</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Search",
+  setup() {
+    return {};
+  },
+});
+</script>
